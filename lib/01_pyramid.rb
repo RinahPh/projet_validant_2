f8a0b2e8fe2541f19422be06b58c5763cf5e617e
Rabeharisoa Rinah Philbert <rabeharisoarinahphilbert@gmail.com>
2019-10-03 17:34:31 +0300

    modification de 01_pyramid.rb

--- lib/01_pyramid.rb
+++ lib/01_pyramid.rb
@@ -21,3 +21,53 @@ end
 
 #execute la methode
 half_pyramid
+
+#2.2.2 Pyramide de Gizeh
+def full_pyramid
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	print "> "
+	ht = gets.to_i
+	#boucle pour cree la pyramide
+	ht.times do |n|
+		#affiche la pyramide complete
+    	print ' ' * (ht - n)
+    	puts '#' * (2 * n + 1)
+  	end
+ 
+end
+
+#execute la fonction
+ full_pyramid
+
+# 2.2.3. Alexandrie, Alexandra
+
+def wtf_pyramid
+	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+	print "> "
+
+	# recupere la valeur entree par l'utilisateur
+	ht = gets.to_i
+
+#verifie si le nombre est impaire
+ if ht % 2 == 1
+ 	#affiche la moitie superieur de la pyramide
+	ht.times do |n|
+		if (ht / 2) + 1 > n
+			print ' ' * (ht - n - 1)
+    		puts '#' * (2 * n + 1)
+    	end
+  	end
+
+  	# affiche la moitie inferieur de la pyramide
+	(ht/2).downto(1).each do |i|
+		print " " * (ht - i)
+		puts "#" * (i * 2 - 1)
+ 	end
+ else
+ 	#affiche message d'erreur si le nombre est impaire
+ 	puts "Entrez nombre impair"		
+ end
+end
+
+# execute la methode
+ wtf_pyramid
